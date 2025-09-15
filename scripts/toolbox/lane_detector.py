056cae4cad8a2395bbd2f7cc4066fd84a84e1d32
Yikai Zeng <yikai.zeng@tu-dresden.de>
2025-09-15 18:04:13 +0200

    new detect method

--- scripts/toolbox/lane_detector.py
+++ scripts/toolbox/lane_detector.py
@@ -3,11 +3,13 @@ import numpy as np
 import os
 
 class LaneDetector:
+    # Essentially it detects everything that is on the lane, including other cars, stop lines, etc.
+    # The naming is a bit misleading.
     def __init__(self, debug=False, visual_debug=False):
+
         self.debug = debug
 
         self.publish_visualization = visual_debug
-        self.scanline_y     = [0.6, 0.7, 0.8]
 
         self.lower_yellow   = (20, 80, 200)
         self.upper_yellow   = (40, 230, 255)
@@ -15,21 +17,23 @@ class LaneDetector:
         self.lower_white    = (0, 0, 200)
         self.upper_white    = (150, 30, 255)
 
-        self.lower_red1 = (0, 100, 100)
-        self.upper_red1 = (10, 255, 255)
+        self.lower_red1     = (0, 100, 100)
+        self.upper_red1     = (10, 255, 255)
 
-        self.lower_red2 = (160, 100, 100)
-        self.upper_red2 = (179, 255, 255)
+        self.lower_red2     = (160, 100, 100)
+        self.upper_red2     = (179, 255, 255)
 
-        self.near_stop_line = False
-        self.near_car       = False
+        self.near_stop_line  = False
+        self.near_car        = False
 
-    def stop_detect(self, hsv_frame):
-        # step 1: remove the upper part of the image
+        self.last_left_boundary  = 0
+        self.last_right_boundary = 320
+
+    def detect_stop_line(self, hsv_frame):
+        
         mask_red1 = cv2.inRange(hsv_frame, self.lower_red1, self.upper_red1)
         mask_red2 = cv2.inRange(hsv_frame, self.lower_red2, self.upper_red2)
         mask_red = cv2.bitwise_or(mask_red1, mask_red2)
-
         num_red_pixels = np.count_nonzero(mask_red)
         if self.debug:
             print(f"Number of red pixels detected: {num_red_pixels}")
@@ -49,200 +53,129 @@ class LaneDetector:
             return mask_red, False
         else:
             return False
-
-    def lane_boundaries_detect(self, frame, search_range_y = None, search_range_x = None):
-        result = []
+        
+    def image_process(self,frame):
         # step 1: remove the upper part of the image
         height, width = frame.shape[:2]
         roi = np.zeros_like(frame)
-        cutting_height = int(height * 0.5)
+        cutting_height = int(height * 0.25)
         roi[cutting_height:, :] = frame[cutting_height:, :]
         frame = roi.copy()
-        # step 2: convert to HSV and create masks for yellow and white lanes
+
+        # step 2: convert to HSV 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask_yellow = cv2.inRange(hsv, self.lower_yellow, self.upper_yellow)
-        mask_white = cv2.inRange(hsv, self.lower_white, self.upper_white)
 
         if self.publish_visualization:
-            mask_red, self.near_stop_line = self.stop_detect(hsv)
-            if mask_red is None:
-                mask_red = np.zeros_like(mask_yellow)
+            self.mask_red,self.near_stop_line = self.detect_stop_line(hsv)
+            self.lane_mask, left_boundary, right_boundary, scan_y = self.detect_lane_boundaries(hsv)
         else:
-            self.near_stop_line = self.stop_detect(hsv)
+            self.near_stop_line = self.detect_stop_line(hsv)
+            left_boundary, right_boundary = self.detect_lane_boundaries(hsv)
         
-        # step 3: search for lane boundaries
-        if search_range_x is None:
-            search_range_x = (0, width)
-        if search_range_y is None:
-            search_range_y = (0.5*height - 40, 0.5*height)
+        if left_boundary is not None:
+            self.last_left_boundary = left_boundary
+        if right_boundary is not None:
+            self.last_right_boundary = right_boundary
 
-        for search_y in np.arange(search_range_y[0], search_range_y[1], 10):
-            scan_y = int(search_y)
-            yellow_line   = mask_yellow[scan_y, search_range_x[0]:search_range_x[1]]
-            if np.count_nonzero(yellow_line) >= 5:
-                yellow_indices = np.where(yellow_line > 0)[0]
-                clusters = self.cluster_indices(yellow_indices, gap=1)
-                for cluster in clusters:
-                    if len(cluster) >= 5:
-                        if self.debug:
-                            print(f"Continuous yellow line found at y={scan_y}, x={cluster[0]+search_range_x[0]} to {cluster[-1]+search_range_x[0]}")
-                    # this is a valid cluster for yellow lane 
-                        result.append((scan_y, np.mean(cluster),1))  # 1 for yellow lane
-                        break
-                    else: continue
-            else: continue
-            
-        for search_y in np.arange(search_range_y[0], search_range_y[1], 10):
-            scan_y = int(search_y)
-            white_line   = mask_white[scan_y, search_range_x[0]:search_range_x[1]]
-            if np.count_nonzero(white_line) >= 5:
-                white_indices = np.where(white_line > 0)[0]
-                clusters = self.cluster_indices(white_indices, gap=1)
-                for cluster in clusters:
-                    if len(cluster) >= 5:
-                        if self.debug:
-                            print(f"Continuous white line found at y={scan_y}, x={cluster[0]+search_range_x[0]} to {cluster[-1]+search_range_x[0]}")
-                        # this is a valid cluster for white lane 
-                        result.append((scan_y, np.mean(cluster),2))
-                    else: continue
-            else: continue
+        if self.publish_visualization:
+
+            cv2.circle(frame, (int(self.last_left_boundary), scan_y), 4, (255, 255, 0), -1)
+            cv2.circle(frame, (int(self.last_right_boundary), scan_y), 4, (0, 0, 255), -1)
+
+            self.visualize(frame, self.lane_mask, self.mask_red)
+
+
         
-        if self.debug:
-            print(f"Detected lane boundaries: {len(result)}")
-            for r in result:
-                print(f"y={r[0]}, x={r[1]}, type={r[2]}")
-        if self.publish_visualization:
-            # Draw detected lane boundaries
-            for y, x, lane_type in result:
-                color = (0, 255, 0) if lane_type == 1 else (255, 255, 0)  # Green for yellow, Cyan for white
-                cv2.circle(frame, (int(x), int(y)), 4, color, -1)
-            image_center = width // 2
-            cv2.line(frame, (image_center, 0), (image_center, cutting_height), (100, 100, 255), 1)
-            return frame, mask_yellow, mask_white,result
-        return result
-         
+    def detect_lane_boundaries(self, hsv_frame):
+        # we have made changes on the layout of lanes
+        # there should always be a yellow line on the left side of the lane, 
+        # but it can be next to a whitle line, which is the right boundary of the other lane
+        # therefore we need to try distinguish the two lines
 
-    def center_detect(self, frame):
-        # step 1: remove the upper part of the image
-        height, width = frame.shape[:2]
-        roi = np.zeros_like(frame)
-        cutting_height = int(height * 0.5)
-        roi[cutting_height:, :] = frame[cutting_height:, :]
-        frame = roi.copy()
-        # step 2: convert to HSV and create masks for yellow and white lanes
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # if self.debug:
-        #     interested_point = (129,213)
-        #     print(f"HSV value at {interested_point}: {hsv[interested_point[1], interested_point[0]]}")
-        mask_yellow = cv2.inRange(hsv, self.lower_yellow, self.upper_yellow)
-        mask_white = cv2.inRange(hsv, self.lower_white, self.upper_white)
-        lane_mask = cv2.bitwise_or(mask_yellow, mask_white)
-        if self.publish_visualization:
-            mask_red, self.near_stop_line = self.stop_detect(hsv)
-            if mask_red is None:
-                mask_red = np.zeros_like(lane_mask)
-        else:
-            self.near_stop_line = self.stop_detect(hsv)
+        left_yellow_boundary = None
+        right_white_boundary = None
 
-        contours, _ = cv2.findContours(lane_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            x, y, w, h = cv2.boundingRect(cnt)
-            aspect_ratio = float(w) / h if h != 0 else 0
-            mask_roi = lane_mask[y:y+h, x:x+w]
-            # hollow_score = 1.0 - (np.count_nonzero(mask_roi) / (w * h)) if w * h > 0 else 0
+        mask_yellow = cv2.inRange(hsv_frame, self.lower_yellow, self.upper_yellow)
+        mask_white  = cv2.inRange(hsv_frame, self.lower_white, self.upper_white)
 
-            if area < 280:
-                continue
-
-            if self.debug:
-                # print(f"Contour: Area={area}, Aspect Ratio={aspect_ratio:.2f}")
-                label = f"A={int(area)} AR={aspect_ratio:.2f}"
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 165, 255), 2)
-                cv2.putText(frame, label, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 165, 255), 1)
-            
-            lane_mask[y:y+h, x:x+w] = cv2.bitwise_or(lane_mask[y:y+h, x:x+w], mask_roi)
-
-        centers = [None, None, None]  # Initialize centers for each scanline
-        # check yellow
-
-        for i,y in enumerate(self.scanline_y):
-            center = None
-            scan_y = int(height * y)
-            yellow_indices  = np.where(mask_yellow[scan_y, :] > 0)[0]
-            white_indices   = np.where(mask_white[scan_y, :] > 0)[0]
-            indices         = np.where(lane_mask[scan_y, :] > 0)[0]
-            
+        scan_height_based = int(hsv_frame.shape[0]/2) + 10
+        for row_offset in range(-10,20,5):
+            scan_height = scan_height_based + row_offset
+            row = mask_yellow[scan_height,:]
+            yellow_indices = np.where(row > 0)[0]
+            # we should be careful that there might be multiple yellow segments
             if len(yellow_indices) > 0:
-                # if we have yellow points, we can solely rely on them
-                yellow_clusters = self.cluster_indices(yellow_indices)
-                if self.debug:
-                    for cluster in yellow_clusters:
-                        print(f"Yellow cluster at y={y}: {cluster}")
-                yellow_clusters = [c for c in yellow_clusters if len(c) >= 4]
-                if len(yellow_clusters) == 0:
-                    continue
-                left_cluster    = yellow_clusters[0]
-                left_point      = np.mean(left_cluster)
+                yellow_clusters = self._detect_clusters(yellow_indices)
+                if len(yellow_clusters) > 0:
+                    # choose the leftest cluster
+                    left_yellow_cluster = yellow_clusters[0]
+                    left_yellow_boundary = int(np.mean(left_yellow_cluster))
 
-                # Determine right_point using white_indices if available, else estimate
-                right_point = left_point + self.lane_width_at(y)
-                if len(white_indices) > 0:
-                    right_clusters = self.cluster_indices(white_indices)
-                    if right_clusters:
-                        right_cluster = right_clusters[-1]
-                        if len(right_cluster) > 10:
-                            right_point = np.mean(right_cluster)
-
-                if self.debug:
-                    print(f"Yellow clusters at y={y}: {left_point}, Right point: {right_point}")
-                
-                center = int((left_point + right_point) / 2)
-                centers[i] = center
-            else:
-                # no yellow points, we need to find the center based on the lane mask
-                if len(indices) > 0:
-                    clusters = self.cluster_indices(indices)
-                    clusters = [c for c in clusters if len(c) >= 10]
-                    if len(clusters) >= 2:
-                        # if we have two clusters, we can find the center
-                        left_cluster    = clusters[0]
-                        right_cluster   = clusters[-1]
-                        left_point      = np.mean(left_cluster)
-                        right_point     = np.mean(right_cluster)
-                        if self.debug:
-                            print(f"Left cluster at y={scan_y}: {left_point}, Right cluster: {right_point}")
-                        center = int((left_point + right_point) / 2)
-                        centers[i] = center
+                    # then we find the right yellow one if there are more than 2 clusters
+                    if len(yellow_clusters) > 1:
+                        right_yellow_cluster = yellow_clusters[1]
+                        right_yellow_boundary = int(np.mean(right_yellow_cluster))
                     else:
-                        if self.debug:
-                            print(f"Not enough clusters found at y={y}, clusters found: {len(clusters)}")
-                        continue
-                else:
-                    if self.debug:
-                        print(f"No sufficient points detected at y={y}")
+                        right_yellow_boundary = 360 # if there is no right yellow line, we set it to a large value
+            else:
+                continue
+                    
+        # we should only care the while line in btween the two yellow lines
+        if left_yellow_boundary is None:
+            # unable to find left yellow line
+            left_yellow_boundary = 0
+            right_yellow_boundary = 360
 
-            
-            if self.publish_visualization:
-                if center is not None:
-                    cv2.circle(frame, (center, scan_y), 4, (0, 255, 0), -1)
-                    cv2.circle(frame, (int(left_point), scan_y), 4, (255, 255, 0), -1)
-                    cv2.circle(frame, (int(right_point),scan_y), 4, (0, 0, 255), -1)
-                    cv2.line(frame, (0, scan_y), (width, scan_y), (255, 0, 0), 1)
-                    image_center = width // 2
-                    cv2.line(frame, (image_center, 0), (image_center, cutting_height), (100, 100, 255), 1)
+        row = mask_white[scan_height,left_yellow_boundary:right_yellow_boundary]
+        white_indices = np.where(row > 0)[0]
+        
+        if len(white_indices) > 0:
+            white_clusters = self._detect_clusters(white_indices)
+            if len(white_clusters) == 1:
+                # chose the one nearest to the right yellow line
+                right_white_cluster = white_clusters[-1]
+                right_white_boundary = int(np.mean(right_white_cluster)) + left_yellow_boundary
+            elif len(white_clusters) > 1:
+                right_white_cluster = white_clusters[-1]
+                right_white_boundary = int(np.mean(right_white_cluster)) + left_yellow_boundary
+                if left_yellow_boundary == 0:
+                    # this is very likely a color reflection problem, we can then gusee
+                    left_yellow_boundary = int(np.mean(white_clusters[0]))
+
         if self.debug:
-            print("Centers at scanlines:", centers)
+            print(f"Left boundary: {left_yellow_boundary}, Right boundary: {right_white_boundary}")
         if self.publish_visualization:
-            return frame, lane_mask, mask_red, centers
-        else:
-            return centers
+            combined_mask = mask_yellow # optinal which one
+            return combined_mask, left_yellow_boundary, right_white_boundary, scan_height
 
+        return left_yellow_boundary, right_white_boundary
+
+    def _detect_clusters(self,row):
+        # this is a 1D clusters of indexes like [ 1,2,3, 20,21,22, 50,51]
+        # we need to break them into [[1,2,3], [20,21,22], [50,51]]
+        clusters = []
+        break_gap = 20
+        current_cluster = [row[0]] if len(row) > 0 else []
+        for i in range(1,len(row)):
+            if row[i] > row[i-1] + break_gap:
+                # break here
+                clusters.append(current_cluster)
+                current_cluster = [row[i]]
+            else:
+                current_cluster.append(row[i])
+        clusters.append(current_cluster)
+        # remove clusters too small, set a length threshold
+        clusters = [c for c in clusters if len(c) >= 5]
+
+        return clusters
+    
     def visualize(self, frame, mask, edges):
         import matplotlib.pyplot as plt
-        if not self.debug:
+        if not self.publish_visualization:
             return
+        
         fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+
         axs[0].imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         axs[0].set_title("Lane Detection Result")
         axs[1].imshow(mask, cmap='gray')
@@ -253,31 +186,11 @@ class LaneDetector:
             ax.axis('off')
         plt.tight_layout()
         plt.show()
-
-    def lane_width_at(self, scan_y):
-        scan_pos = np.array([0.6, 0.7, 0.8])
-        width_px = np.array([170, 220, 270])
-        return int(np.interp(scan_y, scan_pos, width_px))
-    
-    def cluster_indices(self, indices, gap=20):
-        clusters = []
-        cluster = [indices[0]]
-        for i in range(1, len(indices)):
-            if indices[i] - indices[i-1] <= gap:
-                cluster.append(indices[i])
-            else:
-                clusters.append(cluster)
-                cluster = [indices[i]]
-        clusters.append(cluster)
-        return clusters
-
-
 if __name__ == "__main__":
-    IMAGE_PATH = "test/test_img/image10.png"
+    IMAGE_PATH = "test/test_img/image04.png"
     frame = cv2.imread(IMAGE_PATH)
     if frame is None:
         raise FileNotFoundError(f"Cannot load image: {IMAGE_PATH}")
 
     detector = LaneDetector(debug=True,visual_debug=True)
-    frame_out, mask_out, edges_out, centers = detector.center_detect(frame)
-    detector.visualize(frame_out, mask_out, edges_out)
+    detector.image_process(frame)
